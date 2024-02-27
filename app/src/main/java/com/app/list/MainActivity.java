f1f15c6815a136ba671662858918289c80a24f29
Younes Makhchan <younessmakhchane@gmail.com>
2024-02-27 15:45:09 +0100

    List View Implementation

---
+++ app/src/main/java/com/app/list/MainActivity.java
@@ -0,0 +1,41 @@
+package com.app.list;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonAdd=findViewById(R.id.buttonAdd);
+
+        EditText textViewName=findViewById(R.id.editTextName);
+        EditText textViewEmail=findViewById(R.id.editTextEmail);
+        ListView listView=findViewById(R.id.listView);
+
+        List<String> emails =new ArrayList<String>();
+        ArrayAdapter adaper=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        listView.setAdapter(adaper);
+        Log.d("onCreate", "onCreate: lunch ");
+        buttonAdd.setOnClickListener(view -> {
+            String name=textViewName.getText().toString();
+            String email=textViewEmail.getText().toString();
+            emails.add("name:"+name+"\n"+"Email:"+email);
+            adaper.notifyDataSetChanged();
+
+        });
+
+    }
+}
